e9285766217eef08051a2423176caa2f70fcd4ec
dhaboav <dhaboav@gmail.com>
2023-11-30 17:35:09 +0700

    add class Times

--- script/password_generator.py
+++ script/password_generator.py
@@ -2,6 +2,25 @@ from datetime import datetime as time
 from random import randint
 
 
+class Times:
+    def __init__(self):
+        self.date = time.now()
+
+    def log_time(self):
+        time_log = f"{self.date:%h-%d-%Y}, {self.date:%H:%M:%S}"
+        return time_log
+    
+    def date_time(self):
+        year = self.date.year
+        months = [
+            "January", "February", "March",
+            "April", "May", "June", 
+            "July", "August", "September", 
+            "October", "November", "December"
+        ]
+        return months, year
+
+
 def random_number(length):
     if length <= 0:
         raise ValueError("Negative length")  
@@ -10,22 +29,11 @@ def random_number(length):
     return randint(lower_bound, upper_bound)
 
 
-def generate_password(rand_int):
-    date = time.now()
-    year = date.year
-    categories = ["Private", "School", "Microsoft"]
-    months = [
-        "January", "February", "March",
-        "April", "May", "June", 
-        "July", "August", "September", 
-        "October", "November", "December"
-    ]
-    
-
+def generate_password(length, categories, months, year):
     for category in categories:
         print(f"{category}:")
         for month in months:
-            number = random_number(rand_int)
+            number = random_number(length)
             print(f"{month}_{number}_{year}X_{category}")
         print()
 
@@ -33,4 +41,7 @@ def generate_password(rand_int):
 
 
 if __name__ == "__main__":
-    generate_password(10)
+    clock = Times()
+    month, year = clock.date_time()
+    categories = ["Private", "Schools", "Microsoft"]
+    generate_password(6, categories, month, year)
