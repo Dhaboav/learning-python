f9041ffc08f1bec442958fab6397c26f7fcc4e9b
dhaboav <dhaboav@gmail.com>
2023-11-30 18:29:12 +0700

    add save feature txt

--- script/password_generator.py
+++ script/password_generator.py
@@ -21,6 +21,16 @@ class Times:
         return months, year
 
 
+class Save:
+    def log(self, path, log_time):
+        with open(path, "w") as file:
+            file.write(f"{log_time}")
+
+    def write_data(self, file_name, data):
+        with open(file_name, "a") as file:
+            file.write("\n" + data)
+
+
 def random_number(length):
     if length <= 0:
         raise ValueError("Negative length")  
@@ -28,20 +38,30 @@ def random_number(length):
     upper_bound = (10 ** length) - 1
     return randint(lower_bound, upper_bound)
 
-
 def generate_password(length, categories, months, year):
+    passwords = []
     for category in categories:
-        print(f"{category}:")
+        passwords.append(f"\n{category}:")
         for month in months:
             number = random_number(length)
-            print(f"{month}_{number}_{year}X_{category}")
-        print()
-
+            generated_password = f"{month}_{number}_{year}X_{category}"
+            passwords.append(generated_password)
+    return passwords
 
 
 
 if __name__ == "__main__":
+
+    # Generating password
     clock = Times()
     month, year = clock.date_time()
+    log_time = clock.log_time()
     categories = ["Private", "Schools", "Microsoft"]
-    generate_password(6, categories, month, year)
+    passwords = generate_password(5, categories, month, year)
+
+    # Save the generated password to txt
+    file_folder = r"script\password.txt"
+    database = Save()
+    database.log(file_folder, log_time)
+    for password in passwords:
+        database.write_data(file_folder, password)
